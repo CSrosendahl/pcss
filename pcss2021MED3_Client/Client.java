fc8ca3c760986621c5e1647e3afc7321a6df3769
Amarck20 <75370400+Amarck20@users.noreply.github.com>
2021-09-22 11:45:43 +0200

    First steps in creating client. Write name, not working

--- pcss2021MED3_Client/Client.java
+++ pcss2021MED3_Client/Client.java
@@ -1,4 +1,30 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        boolean connect = true;
+
+        try {
+            Socket connectToServer = new Socket("localhost", 6969);
+            DataInputStream isFromServer = new DataInputStream(connectToServer.getInputStream());
+            DataOutputStream osToServer = new DataOutputStream(connectToServer.getOutputStream());
+            if(connect)
+                System.out.print("Enter you alias: ");
+            String alias = input.nextLine();
+
+            osToServer.writeChars(alias);
 
 
+        } catch (IOException ex) {
+            System.out.println(ex.toString() + '\n');
+        }
+
+        //System.out.print("Welcome " + alias);
+
+
+    }
 }
